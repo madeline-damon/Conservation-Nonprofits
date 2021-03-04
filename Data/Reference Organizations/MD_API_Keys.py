bdf81586084b70c8a7eb4591c988728391f5afbd
madeline-damon <69983896+madeline-damon@users.noreply.github.com>
2021-03-04 16:44:44 -0700

    Add files via upload

---
+++ Data/Reference Organizations/MD_API_Keys.py
@@ -0,0 +1,7 @@
+api_key = "x7Of9hfAAOhEvP7xsGbQ96TL0"
+api_key_secret = "m46SWBwJChOAqgyROHLSSKBvigeo0uGHborKKikXyOnZsMPwZV"
+
+access_token = "1287435553223442432-oRqHjU4PEzeYbWZUlRRBAkduHfeC6P"
+access_token_secret = "6qcn9Oirl24li8ruU6n0BpeeyX9AhPbIuB6oj1an2cMrb"
+
+
